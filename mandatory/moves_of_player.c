dc787006aee4250d7829c94fb6736f65b663c31c
Mustapha Essalih <messalih@e3r7p15.1337.ma>
2022-09-08 14:38:37 +0100

    directions edit

--- mandatory/moves_of_player.c
+++ mandatory/moves_of_player.c
@@ -2,7 +2,7 @@
 
 
 
-void move_player(t_player *p)
+int move_player(t_player *p)
 {
     
     p->distance = malloc(sizeof(double) * p->num_of_rays);
@@ -18,44 +18,32 @@ void move_player(t_player *p)
     float next_x;
     float next_y;
     float move_step = p->walk * p->walk_speed;
-    next_x = p->x +  cos(p->rotation_angle) * move_step; 
-    next_y = p->y + sin(p->rotation_angle) * move_step;
+    if(p->move != 0)
+    {
+        move_step = p->move * ( PI / 2);
+        next_x = p->x +  (cos(p->rotation_angle  + move_step)) ; 
+         
+        next_y = p->y + (sin(p->rotation_angle + move_step)) ;
+    }
+    else
+    {
+        next_x = p->x +  cos(p->rotation_angle) * move_step; 
+        next_y = p->y + sin(p->rotation_angle) * move_step;
+    }
     if(p->cub_info.map[(int)(next_y / TILE_SIZE)][(int)(next_x / TILE_SIZE)] != '1')
     {
         p->x = next_x; 
         p->y = next_y;
     }
     find_intersections(p,0);
-     
+     return 0;
 }
 
 
-int	key_hook_(t_player *p, int keycode)
-{
-    float move_step;
-    
-    if(keycode == 1)// s
-        p->walk = -1;
-    if(keycode == 2)// a
-    {
-        p->move = -1;
-        move_step = p->move * p->walk_speed;
-        p->x += cos(p->rotation_angle) + move_step;
-    }
-    if(keycode == 0) // d
-    {
-        p->move = +1;
-        move_step = p->move * p->walk_speed;
-        p->x += cos(p->rotation_angle) + move_step;
-    }
-    move_player(p);
-    return (0);
-}
+ 
 
 int	key_hook(int keycode, t_player *p)
 {
-    float move_step;
-
     if(keycode == 124) 
     {
         p->turn = +1;
@@ -68,8 +56,31 @@ int	key_hook(int keycode, t_player *p)
     }
     if(keycode == 13)// w
         p->walk = +1;
-
-    return key_hook_(p,keycode);
+    if(keycode == 1)// s
+        p->walk = -1;
+    if(keycode == 0)// a
+        p->move = -1; 
+    if(keycode == 2) // d
+        p->move = +1;
+    return move_player(p);;
+}
+int	key_hook_s(int keycode, t_player *p)
+{
+ 
+    if(keycode == 124)
+        p->turn = 0;
+    if(keycode == 123)
+        p->turn = 0; 
+    if(keycode == 2)
+        p->move = 0;
+    if(keycode == 0) // d
+        p->move = 0; 
+    if(keycode == 13)
+        p->walk = 0; 
+    if(keycode == 1)// s
+        p->walk = 0; 
+     
+    return 0;
 }
 
  
@@ -78,6 +89,7 @@ int moves_of_player(t_player *p)
 {
      
     mlx_hook(p->mlx.mlx_win, 02, 1L<<0, key_hook, p); 
+    mlx_hook(p->mlx.mlx_win, 03, 1L<<0, key_hook_s, p); 
      
 
     return 1;
